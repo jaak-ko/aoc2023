83ab5f4e81cda12ddfb3e55df95f2eca11ea520c
jaakko <jaakko.junttu@tuni.fi>
2023-12-01 17:36:50 +0200

    what was i thinking

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,41 +1,35 @@
 import numpy as np
 
+
 def part_one(lines):
 
-    int_lines = [[i for i in line if i.isdigit()] for line in lines]
-    
+    int_lines = [list(filter(lambda x: x.isdigit(), line)) for line in lines]
+
     return sum([int(line[0] + line[-1]) for line in int_lines])
 
 def part_two(lines):
 
-    digits = {"one": '1', "two": '2', "three": '3', "four": '4', "five": '5', 
-                "six": '6', "seven": '7', "eight": '8', "nine": '9', "zero": '0'}
-
-    new_lines = []
-    for line in lines:
-        new_line = []
-
-        for f in range(len(line.strip())):
-            if line[f].isdigit():
-                new_line.append(line[f])
-                continue
-
-            for i in [3, 4, 5]:
-                if line[f:f+i] in digits.keys():
-                    new_line.append(digits[line[f:f+i]])
-                    break
-
-        new_lines.append(new_line)  
-
-    return sum([int(line[0] + line[-1]) for line in new_lines])
-
+    for i in range(len(lines)):
+        lines[i] = list(filter(lambda x: x.isdigit(), \
+                        lines[i].replace('one', 'one1one').\
+                        replace('two', 'two2two').\
+                        replace('three', 'three3three').\
+                        replace('four', 'four4four').\
+                        replace('five', 'five5five').\
+                        replace('six', 'six6six').\
+                        replace('seven', 'seven7seven').\
+                        replace('eight', 'eight8eight').\
+                        replace('nine', 'nine9nine')))
+                        
+        
+    return sum([int(line[0] + line[-1]) for line in lines])
 
 def main():
 
     with open("day_1/input.txt", 'r') as f:
         
-        lines = f.readlines()
-        print(f"part 1: {part_one(lines)}, part 2: {part_two(lines)}")
+        lines = [line.strip() for line in f]
+        print(f"part 1: , part 2: {part_two(lines)}")
 
 if __name__ == "__main__":
     main()
--- template.py
+++ template.py
@@ -1,15 +1,17 @@
 import numpy as np
 
-def part_one(data):
+def part_one(lines):
     pass
 
-def part_two(data):
+def part_two(lines):
     pass
 
 def main():
 
-    with open("input.txt", 'r') as f:
-        pass
+     with open("/input.txt", 'r') as f:
+        
+        lines = f.readlines()
+        print(f"part 1: {part_one(lines)}, part 2: {part_two(lines)}")
 
 if __name__ == "__main__":
     main()
