babacaed16ee7c50176641ad0506863cdd4273fe
jaakko <jaakko.junttu@tuni.fi>
2023-12-17 18:59:51 +0200

    initial for day 8 pt2

--- day_8/day_8.py
+++ day_8/day_8.py
@@ -1,21 +1,35 @@
 import numpy as np
 import re
 from collections import deque
+from math import lcm
 
 def part_one(nodes, turns):
 
     current = "AAA"
     amount = 0
-    while True:
+    while current != "ZZZ":
         current = nodes[current][0] if turns[0] == 'L' else nodes[current][1]
         amount += 1
         turns.rotate(-1)
-        if current == "ZZZ":
-            return amount
+
+    return amount
         
 
 def part_two(nodes, turns):
-    pass
+
+    starts = [node for node in nodes if node.endswith('A')]
+    first_zs = []
+    amount = 0
+    
+    while len(first_zs) < len(starts):
+        for idx in range(len(starts)):
+            starts[idx] = nodes[starts[idx]][0] if turns[0] == 'L' else nodes[starts[idx]][1]
+            if starts[idx].endswith('Z'):
+                first_zs.append(amount+1)
+        turns.rotate(-1)
+        amount += 1
+
+    return lcm(*first_zs)
 
 def main():
 
@@ -29,7 +43,7 @@ def main():
             ls = re.findall(r"\b[A-Z]{3}\b", row)
             nodes.update({ls[0]: (ls[1], ls[2])})
 
-        print(f"part 1: {part_one(nodes, turns)}\npart 2: {part_two(nodes, turns)}")
+        print(f"part 1: {part_one(nodes, turns)} \npart 2: {part_two(nodes, turns)}")
 
 if __name__ == "__main__":
     main()
